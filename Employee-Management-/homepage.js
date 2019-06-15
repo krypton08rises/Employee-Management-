bce0c914ec53404cf4638d6dfee3902ec670215f
Kushagra Raina <kushagra.raina@gmail.com>
2019-06-15 17:24:11 +0530

    adding jquery to homepage

---
+++ Employee-Management-/homepage.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('p').click(function(){
+    console.log($(this).text());
+    $(this).text('Hello<BR>World');
+  });
+  $('.dashboard').mouseenter(function(){
+    console.log('this works!');
+  });
+  %(document).scroll(function(){
+    %('.login').css("border","1px solid blue");
+  });
+});
