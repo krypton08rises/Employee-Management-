528ec047688ffd51cf0162fff3091d2f61d41e59
Kushagra Raina <kushagra.raina@gmail.com>
2019-06-27 16:41:41 +0530

    added some functionalities to apply leave

--- Employee-Management-/UI/USERS/Payroll/user-template-payroll_management.js
+++ Employee-Management-/UI/USERS/Payroll/user-template-payroll_management.js
@@ -1,9 +1,23 @@
-$(document).ready(function(){
-    window.onload = function() {
-                var d = new Date();
-                var n = d.getMonth();
-                what(n);
-                function what(n){
-                    document.getElementById('.month').innerHTML = n;
-                };
-            }});
+
+function mymonth(){
+  document.write(5);
+  /*
+  var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+  var d = new Date();
+  var n = month[d.getMonth()];
+  document.write(n);
+  console.log("n");
+  */
+}
